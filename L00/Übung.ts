8641a44d69d265475553a55314663054e5a999ca
colinechen <coline.drechsel@gmail.com>
2024-03-28 11:54:17 +0100

    EIA

---
+++ L00/Übung.ts
@@ -0,0 +1,9 @@
+/*
+let x: string | boolean | number = 1;
+x = "Hallo";
+console.log(x)
+*/
+
+
+let a: number[] = [7, true, "Hallo"] /* nur Zahlen wegen number*/
+let a: (number | string) [] = [7, 10, "Hallo"]
