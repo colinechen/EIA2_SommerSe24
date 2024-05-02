0975d989e41d386f5dece9053e4b87a80a969256
colinechen <coline.drechsel@gmail.com>
2024-05-02 22:54:44 +0200

    EIA

--- L03/formElement.js
+++ L03/formElement.js
@@ -7,24 +7,24 @@ var Einkaufsliste;
         generateContent(Einkaufsliste.data);
         //Event Listener click erstellt neues Produkt wenn aktiviert
         function neuesProdukt() {
-            const container = document.querySelector('main');
+            const container = document.getElementById('neuesProdukt');
             const neuesDiv = document.createElement('div');
             neuesDiv.className = 'box';
             // Auswahl Containers, in den neues Div erstellt wird wenn event listener auslöst
             neuesDiv.innerHTML = `
                 <form>
                     Ware<br>
-                    <input type="text" name="Ware" id="Ware"><br>
+                    <input type="text" name="Ware" id="box"><br>
                     Menge<br>
-                    <input type="number" name="Menge" id="Menge"><br>
+                    <input type="number" name="Menge" id="box"><br>
                     Einheit<br>
-                    <input type="text" name="Einheit" id="Einheit"><br>
+                    <input type="text" name="Einheit" id="box"><br>
                     Kommentar<br>
-                    <input type="text" name="kommentar" id="kommentar"><br>
+                    <input type="text" name="kommentar" id="box"><br>
                     letzter Kauf<br>
-                    <input type="date" name="letzter Kauf" id="letzterKauf"><br>
-                    gekauft<input type="checkbox" name="gekauft" id="gekauft">
-                    löschen<input type="button" name="löschen" class="loeschen">
+                    <input type="date" name="letzter Kauf" id="box"><br>
+                    gekauft<input type="checkbox" name="gekauft" id="box">
+                    löschen<input type="button" name="löschen" id="box">
                 </form>
             `; //Bestimmung der inneren HTML der neuen Div 
             if (container) {
@@ -60,29 +60,31 @@ var Einkaufsliste;
             const eintragId = "Eintrag_" + Date.now();
             //HTML für neuen Eintrag
             const newEintrag = `
-            <div class="Ware" id=${eintragId}>
-                <span class="Produkt">${_data.Ware}</span>
-                <div class="MengeDiv">
+            <div class="box" id=${eintragId}>
+                <span class="box">${_data.Ware}</span>
+                <div class="box">
                     <label for="Menge${eintragId}">Menge</label>
                     <input type="number" name="Ware" id="Menge${eintragId}" list="numbers" class="smallerInput" value="${_data.Menge}">
                 </div>
-                <div class="letzterKaufDiv">
+                <div class="box">
                     <span> </span>
                     <span id="letzterKauf${eintragId}">${_data.letzterKauf}</span>
                 </div>
-                <div class="KommentarDiv">
+                <div class="box">
                     <label for="Kommentar${eintragId}">Kommentar</label>
                     <input type="textarea" name="Kommentar" id="Kommentar${eintragId}" value="${_data.Kommentar}">
                 </div>
-                <div class="gekauftDiv">
+                <div class="box">
                     <label for="gekauft${eintragId}">gekauft</label>
                     <input type="checkbox" name="gekauft" id="gekauft${eintragId}" ${_data.gekauft ? "gekauft" : ""}>
                 </div>
-                <button type="button" class="löschen" id="löschen${eintragId}">-</button>
+                <button type="button" class="neu" id="löschen${eintragId}">-</button>
             </div>
         `;
+            let testDiv = document.createElement("div");
+            EintragDiv.appendChild(testDiv);
             //Eintrag in DOM einfügen
-            EintragDiv.insertAdjacentHTML("beforeend", newEintrag);
+            testDiv.insertAdjacentHTML("beforeend", newEintrag);
             //LÖschen-Button auswählen + Event Handling für Click
             let newEintragButton = document.querySelector("#löschen" + eintragId);
             function deleteEintrag(_event) {
---
+++ L08/generativeKunst.js
@@ -0,0 +1,51 @@
+"use strict";
+var GenerativeKunst;
+(function (GenerativeKunst) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        const canvas = document.querySelector("canvas");
+        if (!canvas) {
+            console.error("Canvas-Element wurde nicht gefunden!");
+            return;
+        }
+        const crc2 = canvas.getContext("2d");
+        if (!crc2) {
+            console.error("2D-Kontext konnte nicht initialisiert werden");
+            return;
+        }
+        generateArt(crc2, canvas);
+    }
+    function getRandomColor() {
+        const colors = ['#c084fc', '#5ce1e6']; // Lilan und Türkis
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+    function drawLine(crc2, canvas) {
+        const x0 = Math.random() * canvas.width;
+        const y0 = Math.random() * canvas.height;
+        const x1 = Math.random() * canvas.width;
+        const y1 = Math.random() * canvas.height;
+        crc2.strokeStyle = getRandomColor();
+        crc2.lineWidth = Math.random() * 10;
+        crc2.beginPath();
+        crc2.moveTo(x0, y0);
+        crc2.lineTo(x1, y1);
+        crc2.stroke();
+    }
+    function drawCircle(crc2, canvas) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        const radius = Math.random() * 100;
+        crc2.fillStyle = getRandomColor();
+        crc2.beginPath();
+        crc2.arc(x, y, radius, 0, Math.PI * 2, false);
+        crc2.fill();
+    }
+    function generateArt(crc2, canvas) {
+        crc2.clearRect(0, 0, canvas.width, canvas.height);
+        const elementsCount = Math.floor(Math.random() * 100) + 50;
+        for (let i = 0; i < elementsCount; i++) {
+            Math.random() > 0.5 ? drawLine(crc2, canvas) : drawCircle(crc2, canvas);
+        }
+    }
+})(GenerativeKunst || (GenerativeKunst = {}));
+//# sourceMappingURL=generativeKunst.js.map
